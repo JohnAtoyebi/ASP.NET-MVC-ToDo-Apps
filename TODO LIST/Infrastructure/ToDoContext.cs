c3e2a3d8f6c3b1b09561a41dce38c50d2862de04
JohnAtoyebi <atoyebijohn1@gmail.com>
2022-10-28 23:58:46 +0100

    first commit

---
+++ TODO LIST/Infrastructure/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TODO_LIST.Models;
+
+namespace TODO_LIST.Infrastructure
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<TodoList> ToDoList { get; set; }
+
+    }
+}
---
+++ TODO LIST/Models/TodoList.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODO_LIST.Models
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
